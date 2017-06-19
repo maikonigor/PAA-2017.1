b5a731533455b300bd752226f4cd27978455726d
Maikon Igor <maikonigor@gmial.com>
2017-06-19 15:13:50 -0300

    arquivo pra ler os resultados e plotar os graficos e tabelas.

---
+++ cutting-stock problem - pso/read_results.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+files = [
+    "FUN_instance1.txt",
+    "FUN_instance2.txt",
+    "FUN_instance5.txt",
+    "FUN_instance7.txt",
+    "FUN_instance9.txt",
+    "FUN_instance12.txt",
+    "FUN_instance13.txt",
+    "FUN_instance14.txt",
+    "FUN_instance17.txt"
+    ]
+data = np.loadtxt(files[0])
+

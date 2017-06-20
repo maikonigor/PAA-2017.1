634687b64937e4b90507a9f860ab16e7bde259cb
Maikon Igor <maikonigor@gmail.com>
2017-06-19 23:07:07 -0300

    Geração do arquivo CSV com os resultados

--- cutting-stock problem - pso/read_results.py
+++ cutting-stock problem - pso/read_results.py
@@ -11,5 +11,21 @@ files = [
     "FUN_instance14.txt",
     "FUN_instance17.txt"
     ]
-data = np.loadtxt(files[0])
+final = np.array(["Instancia","Pior resultado","Pior Tempo", "Melhor Resultado", "Tempo melhor", "Tempo Medio"])
+
+contador = 0
+for file in files:
+    data = np.loadtxt(file)
+    min = data.min(axis=0)
+    max = data.max(axis=0)
+    media = data.mean(axis=0)
+    
+    linha = np.array([str(contador), str(max[0]), str(max[1]/1000), str(min[0]), str(min[1]/1000), str(media[1]/1000)])
+    final = np.vstack((final,linha))
+    contador+=1
+
+print final
+
+np.savetxt("resultados.csv", final,delimiter=";",fmt="%s")
+ 
 

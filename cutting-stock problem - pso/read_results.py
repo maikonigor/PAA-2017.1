6eb080ada026561ce862139b15ab7b772a034b8c
Maikon Igor <maikonigor@gmail.com>
2017-06-19 23:38:17 -0300

    Geração do arquivo CSV com os resultados sumarizados

--- cutting-stock problem - pso/read_results.py
+++ cutting-stock problem - pso/read_results.py
@@ -1,5 +1,20 @@
 import numpy as np
 
+def formatar_tempo(tempo):
+    tempo /= 1000
+    tempo = round(tempo, 2)
+    if(tempo > 60):
+        tempo /= 60
+        tempo = round(tempo, 2)
+        tempo = str(tempo) + "m"
+    else:
+         tempo = str(tempo) + "s"
+    return tempo
+
+def formatar_resultado(resultado):
+    resultado = round(resultado,4) * 100
+    return str(resultado)
+
 files = [
     "FUN_instance1.txt",
     "FUN_instance2.txt",
@@ -11,7 +26,7 @@ files = [
     "FUN_instance14.txt",
     "FUN_instance17.txt"
     ]
-final = np.array(["Instancia","Pior resultado","Pior Tempo", "Melhor Resultado", "Tempo melhor", "Tempo Medio"])
+final = np.array(["Instancia","Pior resultado (\%)", "Melhor Resultado (\%)", "Tempo Medio"])
 
 contador = 0
 for file in files:
@@ -19,8 +34,16 @@ for file in files:
     min = data.min(axis=0)
     max = data.max(axis=0)
     media = data.mean(axis=0)
+
+    p_result = formatar_resultado(max[0])
+    #p_tempo = formatar_tempo(max[1])
     
-    linha = np.array([str(contador), str(max[0]), str(max[1]/1000), str(min[0]), str(min[1]/1000), str(media[1]/1000)])
+    m_result = formatar_resultado(min[0])
+    #m_tempo = formatar_tempo(min[1])
+    
+    t_medio = formatar_tempo(media[1])
+    
+    linha = np.array([str(contador), p_result, m_result, t_medio])
     final = np.vstack((final,linha))
     contador+=1
 
